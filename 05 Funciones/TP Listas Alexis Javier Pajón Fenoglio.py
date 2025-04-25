4f336b1bfd732305e01733ac7474a6de6f4ea369
alexispajon <alexispajon9@gmail.com>
2025-04-25 12:49:34 -0300

    Entrega del TP 5 Listas

---
+++ 05 Funciones/TP Listas Alexis Javier Pajón Fenoglio.py
@@ -0,0 +1,52 @@
+#Actividad 1
+multiplos_de_4 = list(range(0,100,4))
+print(multiplos_de_4)
+
+#Actividad 2
+receta_torta = ["Harina","leche","huevos","azúcar","polvo_de_hornear"]
+print(receta_torta[-2])
+
+#Actividad 3
+lista_vacia = []
+lista_vacia.append("lunes")
+lista_vacia.append("martes")
+lista_vacia.append("miércoles")
+print(lista_vacia)
+
+#Actividad 4
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"
+animales[3] = "oso"
+print(animales)
+
+#Actividad 5
+# Lo que realiza el programa es eliminar el valor más alto de la lista y luego la imprime. En este caso eliminó el número 22.
+
+#Actividad 6
+num_10_30 = list(range(10,31,5))
+print(num_10_30[0:2])
+
+#Actividad 7
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1] = "corsa"
+autos[2] = "renault"
+print(autos)
+
+#Actividad 8
+dobles = []
+dobles.append(5*2)
+dobles.append(10*2)
+dobles.append(15*2)
+print(dobles)
+
+#Actividad 9
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],
+["agua"]]
+compras[2].append("jugo")
+compras[1] [1] = "tallarines"
+compras[0].remove("pan")
+print(compras)
+
+#Actividad 10
+lista_anidada = [[15] , [True], [25.5 , 57.9 , 30.6], [False]]
+print(lista_anidada)
